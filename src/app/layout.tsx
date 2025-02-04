97aa7d2e3f1dbf2348e48eb02ae1efee721e8c7a
Mansory888 <nichita.railean@aol.com>
2025-02-04 14:33:13 +0100

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ import { useState, useEffect } from "react";
 import SplashScreen from "@/components/splashScreen";
 import { Toaster } from "react-hot-toast";
 
+
+export const metadata = {
+  title: 'SOOF Factoring B.V',
+  icons: {
+    icon: '/favicon.ico', 
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
